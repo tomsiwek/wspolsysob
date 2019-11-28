3b7406a217568db271feaa3d3d0765b82a831733
Tomasz Siwek <tsiwek@g.pl>
2019-11-28 18:59:40 +0100

    zopytmalizowano slow.c i slow.py

--- optymalizacja/slow.c
+++ optymalizacja/slow.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 #define M 100
 #define N 100
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+
+double* sin_vals;
+double* cos_vals;
+double** sin_vals_mat;
+double** cos_vals_mat;
+
+void init_sin_cos_table(int n, int m, double* sin_vals, double* cos_vals, double** sin_vals_mat, double** cos_vals_mat)
+{
+    int i, j;
+    double tmp;
+    
+    for(j = 0; j < m; j++)
+    {
+        sin_vals[j] = sin(M_PI*(2.0*j+0.5));
+        cos_vals[j] = cos(M_PI*(2.0*j));
+        for(i = 0; i < n; i++)
+        {
+            tmp = sin(M_PI * i + j*.1);
+            sin_vals_mat[j][i] = tmp*tmp;
+            tmp = cos(M_PI * i + j*.1);
+            cos_vals_mat[j][i] = tmp*tmp;
+        }
+    }
+}
 
 double error(double err, double A, double B)
 {
@@ -20,28 +47,35 @@ double error(double err, double A, double B)
 	return pom;
 }
 
-double modA (int i, int j, double** A){
+double modA(int i, int j, double** A)
+{
 	double pom;
 	if (i<1) return 0;
-	else{
+	else
+    {
 		if (j<1) return 0;
-		else{
-			pom = 0;
-			pom = pom + A[j][i+1] * 0.25 * sin (M_PI*(2.*i+.5));
-		        pom = pom + A[j][i-1] * 0.25 * cos (M_PI*(2.*i));
-			pom = pom + A[j-1][i] * 0.25 * sin (M_PI*(2.*j+.5));
-			pom = pom + A[j+1][i] * 0.25 * cos (M_PI*(2.*j));
+		else
+        {
+            pom = 0.0;
+			pom += A[j][i+1] * sin_vals[i];
+            pom += A[j][i-1] * cos_vals[i];
+            pom += A[j-1][i] * sin_vals[i];
+            pom +=  A[j+1][i] * cos_vals[i];
+            pom *= 0.25;
 		}
 	}
 	return pom;
 }
 
-void startA (int m, int n, double** A){
+void startA(int m, int n, double** A)
+{
 	int i, j;
-	for( i = 0; i < m; i++) {
+	for(i = 0; i < m; i++)
+    {
 		A[0][i] = 100;
 		A[1][i] = 100;
-  		for( j = 2; j < n; j++){
+  		for(j = 2; j < n; j++)
+        {
 			A[j][i] = 10;
 			if (j==0) A[j][i] = 100;
 			if (j==1) A[j][i] = 100;
@@ -49,26 +83,36 @@ void startA (int m, int n, double** A){
 	}
 }
 
-void makeMV(int n, int m, double** A, double** ANEW, double* err){
+void makeMV(int n, int m, double** A, double** ANEW, double* err)
+{
 	int i, j;
 	*err = 0;
-	for (i=1; i<m-1; i++)
-		for (j=1; j<n-1; j++){
+	for (j=1; j<n-1; j++)
+	{
+        for(i=1; i<m-1; i++)
+        {
 			if (i>0 && i<m && j>0 && j<n)
 				ANEW[j][i] = modA(i, j, A);
 			if (i>0 && i<m && j>0 && j<n)
 				*err = error(*err, A[j][i], ANEW[j][i]);
 		}
+    }
 }
 
-void copy(int n, int m, double** A, double** ANEW){
+void copy(int n, int m, double** A, double** ANEW)
+{
 	int i, j;
+    for (j=1; j<n-1; j++)
+    {
         for (i=1; i<m-1; i++)
-		for (j=1; j<n-1; j++)
-			A[j][i] = ANEW[j][i] * ( sin(M_PI * i+j*.1)  * sin(M_PI * i+j*.1) + cos(M_PI * i+j*.1)* cos(M_PI * i+j*.1) );
+        {
+            A[j][i] = ANEW[j][i] *  (sin_vals_mat[j][i]  + cos_vals_mat[j][i]);
+        }
+    }
 }
 
-int main() {
+int main()
+{
 
 int i,j;
 double err, tol;
@@ -84,14 +128,25 @@ Anew = (double**)malloc(n*sizeof(double*));
 for (i=0; i<n; i++)
 	Anew[i] = (double*) malloc(m*sizeof(double));
 
+sin_vals = (double*) malloc(m*sizeof(double));
+cos_vals = (double*) malloc(m*sizeof(double));
+sin_vals_mat = (double**)malloc(n*sizeof(double*));
+cos_vals_mat = (double**)malloc(n*sizeof(double*));
+for (i=0; i<n; i++)
+{
+    sin_vals_mat[i] = (double*) malloc(m*sizeof(double));
+	cos_vals_mat[i] = (double*) malloc(m*sizeof(double));
+}
+init_sin_cos_table(n, m, sin_vals, cos_vals, sin_vals_mat, cos_vals_mat);
+
 iter_max = 1000;
 iter = 0;
 tol = 0.0001;
 err = 1.0;
 
-startA (m, n, A);
+startA(m, n, A);
 
-while ( err > tol && iter < iter_max ) {
+while( err > tol && iter < iter_max ) {
 
   err = 0.0;
 
--- optymalizacja/slow.py
+++ optymalizacja/slow.py
@@ -2,44 +2,53 @@
 
 import cProfile
 import numpy as np
+from scipy import signal
 
 def startA (n):
-	A = np.ones((n, n))
-	for i in range(n):
-		for j in range(n):
-			A[i][j] = 10
-	for i in range(n):
-		A[0, i] = 100
-		A[1, i] = 100
+    # np.diag maybe?
+    A = np.ones((n, n))
+    for i in range(n):
+        for j in range(n):
+            A[i][j] = 10
+    for i in range(n):
+        A[0, i] = 100
+        A[1, i] = 100
 
-	Anew = A.copy()
+    Anew = A.copy()
 
-	return A, Anew
+    return A, Anew
 
 def makeMV(Anew, A, n):
     # Spróbować wyciągnąć 1/4 przed całą macierz
-	for i in range(1,n-1):
-		for j in range(1,n-1):
-				Anew[i, j] = .25 * ( \
-					  A[i-1, j  ] \
-					+ A[i+1, j  ] \
-					+ A[i  , j-1] \
-					+ A[i  , j+1])
+    #for i in range(1, n-1):
+    #    for j in range(1, n-1):
+    #        Anew[i, j] =       (A[i-1, j  ] \
+    #                          + A[i+1, j  ] \
+    #                          + A[i  , j-1] \
+    #                          + A[i  , j+1])
+    #Anew[1:n-1, 1:n-1] *= 0.25
+    # Define kernel for convolution                                         
+    kernel = np.array([[0,1,0],
+                       [1,0,1],
+                       [0,1,0]])
+    # Perform 2D convolution with input data and kernel 
+    Anew[1:n-1, 1:n-1] = signal.convolve2d(A, kernel, mode="valid")*.25
 
 def error(A,Anew):
-	err = 0
-	for i in range(1,n-1):
-		for j in range(1,n-1):
-			pom = np.abs(A[i,j]-Anew[i,j])
-			if pom > err: err = pom
-	return err
-#	return np.abs(A-Anew).max()
+#	err = 0
+#	for i in range(1,n-1):
+#		for j in range(1,n-1):
+#			pom = np.abs(A[i,j]-Anew[i,j])
+#			if pom > err: err = pom
+#	return err
+    return np.abs(A-Anew).max()
 
 def copy(A, Anew, n):
-    # zastąpić biblioteczną funckcją numpy/[deep]copy
-	for i in range(1,n-1):
-		for j in range(1,n-1):
-			A[i,j] = Anew[i,j]
+    # zastąpić biblioteczną funkcją numpy/[deep]copy
+    # macierze numpy mają metodę copy() robiącą to szybko
+    for i in range(1,n-1):
+        for j in range(1,n-1):
+            A[i,j] = Anew[i,j]
 
 pr = cProfile.Profile()
 pr.enable()
@@ -49,13 +58,14 @@ n = 100
 A, Anew = startA(n)
 
 for i in range(1000):
-	makeMV(Anew, A, n)
+    makeMV(Anew, A, n)
 
-	err = error(A, Anew)
+    err = error(A, Anew)
 # print(Anew)
-	copy(A, Anew, n)
+    #copy(A, Anew, n)
+    A = Anew.copy()
 
-	if i%100 ==0: print(err)
+    if i%100 == 0: print(err)
 
 pr.disable()
 pr.print_stats(sort='time')
