0dfb5ae56e2a471298a5a93fb2961a7686483262
Tomasz Siwek <tsiwek@g.pl>
2019-11-28 19:12:47 +0100

    naprawiony slow.c

--- optymalizacja/slow.c
+++ optymalizacja/slow.c
@@ -10,7 +10,6 @@
 
 double* sin_vals;
 double* cos_vals;
-<<<<<<< HEAD
 double** sin_vals_mat;
 double** cos_vals_mat;
 
@@ -31,31 +30,6 @@ void init_sin_cos_table(int n, int m, double* sin_vals, double* cos_vals, double
             cos_vals_mat[j][i] = tmp*tmp;
         }
     }
-=======
-
-double* init_sin_table(int n)
-{
-    int i;
-    double* sin_vals = (double*) malloc(n*sizeof(double));
-
-    for (i = 0; i < n; i++)
-    {
-        sin_vals[i] = sin(M_PI*(2.0*i+0.5));
-    }
-    return sin_vals;
-}
-
-double* init_cos_table(int n)
-{
-    int i;
-    double* cos_vals = (double*) malloc(n*sizeof(double));
-
-    for (i = 0; i < n; i++)
-    {
-        cos_vals[i] = cos(M_PI*(2.0*i));
-    }
-    return cos_vals;
->>>>>>> 433e8a2838e781928fa20832a9cda402124eca43
 }
 
 double error(double err, double A, double B)
@@ -80,7 +54,6 @@ double modA(int i, int j, double** A)
 	else
     {
 		if (j<1) return 0;
-<<<<<<< HEAD
 		else
         {
             pom = 0.0;
@@ -89,15 +62,6 @@ double modA(int i, int j, double** A)
             pom += A[j-1][i] * sin_vals[i];
             pom +=  A[j+1][i] * cos_vals[i];
             pom *= 0.25;
-=======
-		else{
-			pom = 0;
-			pom += A[j][i+1] * sin_vals[i];
-		        pom += A[j][i-1] * cos_vals[i];
-			pom += A[j-1][i] * sin_vals[i];
-			pom += A[j+1][i] * cos_vals[i];
-                        pom *=  0.25;
->>>>>>> 433e8a2838e781928fa20832a9cda402124eca43
 		}
 	}
 	return pom;
@@ -149,62 +113,52 @@ void copy(int n, int m, double** A, double** ANEW)
 
 int main()
 {
+    int i,j;
+    double err, tol;
+    int iter, iter_max;
+    int m = M;
+    int n = N;
+    double** A;
+    double** Anew;
+    A = (double**)malloc(n*sizeof(double*));
+    for (i=0; i<n; i++)
+        A[i] = (double*) malloc(m*sizeof(double));
+    Anew = (double**)malloc(n*sizeof(double*));
+    for (i=0; i<n; i++)
+        Anew[i] = (double*) malloc(m*sizeof(double));
 
-int i,j;
-double err, tol;
-int iter, iter_max;
-int m = M;
-int n = N;
-double** A;
-double** Anew;
-A = (double**)malloc(n*sizeof(double*));
-for (i=0; i<n; i++)
-	A[i] = (double*) malloc(m*sizeof(double));
-Anew = (double**)malloc(n*sizeof(double*));
-for (i=0; i<n; i++)
-	Anew[i] = (double*) malloc(m*sizeof(double));
+    sin_vals = (double*) malloc(m*sizeof(double));
+    cos_vals = (double*) malloc(m*sizeof(double));
+    sin_vals_mat = (double**)malloc(n*sizeof(double*));
+    cos_vals_mat = (double**)malloc(n*sizeof(double*));
+    for (i=0; i<n; i++)
+    {
+        sin_vals_mat[i] = (double*) malloc(m*sizeof(double));
+        cos_vals_mat[i] = (double*) malloc(m*sizeof(double));
+    }
+    init_sin_cos_table(n, m, sin_vals, cos_vals, sin_vals_mat, cos_vals_mat);
 
-<<<<<<< HEAD
-sin_vals = (double*) malloc(m*sizeof(double));
-cos_vals = (double*) malloc(m*sizeof(double));
-sin_vals_mat = (double**)malloc(n*sizeof(double*));
-cos_vals_mat = (double**)malloc(n*sizeof(double*));
-for (i=0; i<n; i++)
-{
-    sin_vals_mat[i] = (double*) malloc(m*sizeof(double));
-	cos_vals_mat[i] = (double*) malloc(m*sizeof(double));
-}
-init_sin_cos_table(n, m, sin_vals, cos_vals, sin_vals_mat, cos_vals_mat);
-=======
-sin_vals = init_sin_table(MAX(n, m));
-cos_vals = init_cos_table(MAX(n, m));
->>>>>>> 433e8a2838e781928fa20832a9cda402124eca43
+    iter_max = 1000;
+    iter = 0;
+    tol = 0.0001;
+    err = 1.0;
 
-iter_max = 1000;
-iter = 0;
-tol = 0.0001;
-err = 1.0;
+    startA(m, n, A);
 
-startA(m, n, A);
+    while( err > tol && iter < iter_max ) {
+        err = 0.0;
 
-while( err > tol && iter < iter_max ) {
+        makeMV(n, m, A, Anew, &err);
 
-  err = 0.0;
+        copy(n, m, A, Anew);
 
-  makeMV(n, m, A, Anew, &err);
+        if( iter++ % 100 == 0 || err <= tol )  printf("%5d, %0.6f\n", iter, err);
+        copy(n, m, A, Anew);
+        if (err < tol)
+            break;
+        if (iter > iter_max)
+            break;
 
-  copy(n, m, A, Anew);
-
-  if ( iter++ % 100 == 0 || err <= tol )  printf("%5d, %0.6f\n", iter, err);
-  copy(n, m, A, Anew);
-  if (err < tol)
-	  break;
-  if (iter > iter_max)
-	  break;
-
-}
-
-
-return 0;
-
+        }
+    return 0;
 }
